8a9e306351f51488ed300f6e405a5133d374ed05
daniel.souza <88326589+daan-ni@users.noreply.github.com>
2022-09-06 18:43:06 -0300

    Add files via upload

---
+++ entity/src/entity/Clientes.java
@@ -0,0 +1,15 @@
+package entity;
+public class Clientes{
+    private int cpf;
+
+    public void Compra(Vendas venda){}
+    public void Cadastrar(Responsavel resp){}
+    public void Alterar(Responsavel resp){}
+
+    public int getCpf(){
+        return cpf;
+    }
+    public void setCpf(int cpf){
+        this.cpf = cpf;
+    }
+}
