184cee796bed8d3227c14fa8398909b8bedaaf39
gustavofernandss <87822363+gustavofernandss@users.noreply.github.com>
2022-09-06 16:10:40 -0800

    Update Equipametos.java

--- entity/src/entity/Equipametos.java
+++ entity/src/entity/Equipametos.java
@@ -1,13 +1,15 @@
 package entity;
 
-public class Equipametos {
+public class Equipamentos {
+
+	
 private String nome, local, responsavel;
 private double preco;
 private int qtd, serialNumber;
 private boolean ativo;	
 
 
-
+// getters
 public String getNome(){
 	return nome;
 }
@@ -27,31 +29,82 @@ public double getpreco(){
 	return preco;
 }
 public int getQtd(){
-	return qtd;
-	
-	
+	return qtd;		
 }
+
+// setters
 public void setNome(String nome){
-	 this.nome = nome;
+if (nome != null) {
+      this.nome = nome;
+    } else {
+      throw new IllegalArgumentException("Nome invalido");
+    }
 }
 public void setSerialNumber(int serialNumber){
-	this.serialNumber = serialNumber ;
+	if (serialNumber > 0) { this.serialNumber = serialNumber;
+}
+else {
+      throw new IllegalArgumentException("Local invalido");
+    }
 }
 public void setLocal(String local){
-	this.local = local;
+if (local != null) {
+      this.local = local;
+    } else {
+      throw new IllegalArgumentException("Local invalido");
+    }
 }
 public void setResponsavel(String responsavel){
-	this.responsavel = responsavel;
+	if (responsavel != null) {
+      this.responsavel = responsavel;
+    } else {
+      throw new IllegalArgumentException("Responsavel invalido");
+    }
 }
 public void setAtivo(boolean ativo){
+	if (ativo == true || ativo == false){
 	this.ativo = ativo;
+	}
+	else {
+      throw new IllegalArgumentException("Ativo invalido");
 }
+}
+
 public void setPreco(double preco){
+	if (preco > 0){
 	this.preco = preco;
+	}
+	else {
+      throw new IllegalArgumentException("Preco invalido");
 }
+}
+
 public void setQtd( int qtd){
+	if (qtd > 0){
 	this.qtd = qtd;
 }
-
-
+else {
+      throw new IllegalArgumentException("Quantidade invalida");
 }
+}
+
+// metodos
+public void getBySerialNumber(int serial){
+	// busca por serial number
+}
+public void GetByNome (String nome){
+	// busca por nome
+}
+public void GetByLocal (String local){
+    // busca por local
+}
+public void GetByResponsavel (String resp){
+	// busca por responsavel
+}
+public void Add(Equipamentos equi){
+	// adiciona equipamentos
+}
+public void Update(Equipamentos equi){
+	// atualiza equipanentos
+}
+
